344a33a85f6a103155581918f30dc95928367f6e
Jonathan Zeray <jonathan.zeray@hotmail.com>
2023-04-25 20:54:32 +0200

    changed variable

--- script.js
+++ script.js
@@ -33,10 +33,10 @@ window.onload = function () {
   window.onscroll = function() {myFunction()};
   
   // Get the navbar
-  var navbar = document.getElementById("navbar");
+  let navbar = document.getElementById("navbar");
   
   // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+let sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
